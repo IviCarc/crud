ad2f31be416b31ecd37688ae70eff8014704f176
ivan-desktop <kovachivanagustin@gmail.com>
2021-12-08 23:15:30 -0300

    variable de entorno en el cliente

--- client/src/components/Formulario.jsx
+++ client/src/components/Formulario.jsx
@@ -1,118 +1,125 @@
-import React from 'react';
-import Usuarios from './Usuarios'
-import axios from 'axios'
-import NuevoUsuario from './NuevoUsuario.jsx';
-import {StyledForm} from './StyledComponents';
+import React from "react";
+import Usuarios from "./Usuarios";
+import axios from "axios";
+import NuevoUsuario from "./NuevoUsuario.jsx";
+import { StyledForm } from "./StyledComponents";
+require("dotenv").config();
 
 export default class Formulario extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      nombre : { value: "", valid : null},
-      apellido : { value: "", valid : null},
-      edad : { value: 0, valid : null},
-      genero : { value: "Hombre", valid : null},
-      users:[],
-    }
-    
-    this.url = "http://localhost:3000/api/users";
-    
-    this.editUser = this.editUser.bind(this);
-    this.deleteUser = this.deleteUser.bind(this);
-    this.getUsers = this.getUsers.bind(this);
-    this.newUser = this.newUser.bind(this);
-    this.cambiosInput = this.cambiosInput.bind(this);
+	constructor(props) {
+		super(props);
+		this.state = {
+			nombre: { value: "", valid: null },
+			apellido: { value: "", valid: null },
+			edad: { value: 0, valid: null },
+			genero: { value: "Hombre", valid: null },
+			users: [],
+		};
 
-    this.regex = {
-      nombre : /^[a-zA-Z]{4,16}$/,
-      apellido : /^[a-zA-Z]{4,16}$/,
-      edad : /^[0-9]{2,2}$/
-    }
-  }
+		this.url = process.env.REACT_APP_URL;
 
-  async componentDidMount() {
-    this.getUsers();
-  }
-  
-  
-  deleteUser = async (e, id) => {
-    e.preventDefault();
-    let url = this.url + "/" + id
-    await axios.delete(url);
-    this.getUsers();
-  }
-  
-  editUser = async (e, usr) => {
-    e.preventDefault();
-    
-    let editData = {
-      "nombre":usr.nombre,
-      "apellido":usr.apellido,
-      "edad":usr.edad,
-      "genero":usr.genero,
-    }
-    await axios.put(this.url + "/" + usr._id, editData);
-    this.getUsers();
-  } 
-  
-  newUser = async (e) => {
-    e.preventDefault();
+		console.log(this.url);
 
-    // CHECK DATA
-    for (const key in this.state) {
-      if (!this.state[key].valid) {alert("Está ingresando mala data"); return}
-    }
+		this.editUser = this.editUser.bind(this);
+		this.deleteUser = this.deleteUser.bind(this);
+		this.getUsers = this.getUsers.bind(this);
+		this.newUser = this.newUser.bind(this);
+		this.cambiosInput = this.cambiosInput.bind(this);
 
-    await axios.post(this.url, {
-      nombre : this.state.nombre.value,
-      apellido : this.state.apellido.value,
-      edad : parseInt(this.state.edad.value),
-      genero : this.state.genero.value
-    });
-    this.getUsers();
-  }
-  
-  getUsers = async () => {
-    const res = await axios.get(this.url);
-    this.setState({users:res.data});
-  }
-  
-  cambiosInput = (e) => {
-    this.setState({
-      [e.target.name] : {...this.state[e.target.name], value:e.target.value} // Actualizo el state sin alterar valid
-    });
-  }
+		this.regex = {
+			nombre: /^[a-zA-Z]{4,16}$/,
+			apellido: /^[a-zA-Z]{4,16}$/,
+			edad: /^[0-9]{2,2}$/,
+		};
+	}
 
-  validar = (e, regex) => {
-    if (regex.test(e.target.value)) {
-      this.setState({
-        [e.target.name] : {...this.state[e.target.name], valid : true}
-      });
-    } else {
-      this.setState({
-        [e.target.name] : {...this.state[e.target.name], valid : false}
-      });
-    }
-  }
-  
-  render () {
-    return (
-      <StyledForm onSubmit={this.newUser}>
+	async componentDidMount() {
+		this.getUsers();
+	}
 
-          <NuevoUsuario 
-          cambiosInput={this.cambiosInput} 
-          state={this.state}
-          validar={this.validar}
-          regex = {this.regex}
-          />
+	deleteUser = async (e, id) => {
+		e.preventDefault();
+		let url = this.url + "/" + id;
+		await axios.delete(url);
+		this.getUsers();
+	};
 
-          <Usuarios 
-          editUser={this.editUser} 
-          deleteUser={this.deleteUser} 
-          users={this.state.users}
-          regex = {this.regex}
-          />
-        </StyledForm>
-    )
-  }
+	editUser = async (e, usr) => {
+		e.preventDefault();
+
+		let editData = {
+			nombre: usr.nombre,
+			apellido: usr.apellido,
+			edad: usr.edad,
+			genero: usr.genero,
+		};
+		await axios.put(this.url + "/" + usr._id, editData);
+		this.getUsers();
+	};
+
+	newUser = async (e) => {
+		e.preventDefault();
+
+		// CHECK DATA
+		for (const key in this.state) {
+			if (!this.state[key].valid) {
+				alert("Está ingresando mala data");
+				return;
+			}
+		}
+
+		await axios.post(this.url, {
+			nombre: this.state.nombre.value,
+			apellido: this.state.apellido.value,
+			edad: parseInt(this.state.edad.value),
+			genero: this.state.genero.value,
+		});
+		this.getUsers();
+	};
+
+	getUsers = async () => {
+		const res = await axios.get(this.url);
+		this.setState({ users: res.data });
+	};
+
+	cambiosInput = (e) => {
+		this.setState({
+			[e.target.name]: {
+				...this.state[e.target.name],
+				value: e.target.value,
+			}, // Actualizo el state sin alterar valid
+		});
+	};
+
+	validar = (e, regex) => {
+		if (regex.test(e.target.value)) {
+			this.setState({
+				[e.target.name]: { ...this.state[e.target.name], valid: true },
+			});
+		} else {
+			this.setState({
+				[e.target.name]: { ...this.state[e.target.name], valid: false },
+			});
+		}
+	};
+
+	render() {
+		return (
+			<StyledForm onSubmit={this.newUser}>
+				<NuevoUsuario
+					cambiosInput={this.cambiosInput}
+					state={this.state}
+					validar={this.validar}
+					regex={this.regex}
+				/>
+
+				<Usuarios
+					editUser={this.editUser}
+					deleteUser={this.deleteUser}
+					users={this.state.users}
+					regex={this.regex}
+				/>
+			</StyledForm>
+		);
+	}
 }
--- client/src/components/NuevoUsuario.jsx
+++ client/src/components/NuevoUsuario.jsx
@@ -1,64 +1,67 @@
 import React from "react";
-import { StyledInput, StyledSelect, StyledBtn, StyledH3 } from "./StyledComponents";
+import {
+	StyledInput,
+	StyledSelect,
+	StyledBtn,
+	StyledH3,
+} from "./StyledComponents";
 
 const NuevoUsuario = (props) => {
-    return (
-        <>
-            <StyledH3>Crear nuevo usuario</StyledH3>
-            <StyledInput 
-            type="text"
-            name="nombre"
-            placeholder="Nombre"
-            onChange={props.cambiosInput}
-            autoComplete="off"
-            onKeyUp={(e) => props.validar(e, props.regex.nombre)}
-            onBlur={(e) => props.validar(e, props.regex.nombre)}
-            required
-            valido={props.state.nombre.valid}
-            ></StyledInput>
+	return (
+		<>
+			<StyledH3>Crear nuevo usuario</StyledH3>
 
-            <StyledInput 
-            type="text"
-            placeholder="Apellido"
-            name="apellido"
-            onChange={props.cambiosInput}
-            autoComplete="off"
-            onKeyUp={(e) => props.validar(e, props.regex.apellido)}
-            onBlur={(e) => props.validar(e, props.regex.apellido)}
-            required
-            valido={props.state.apellido.valid}
-            ></StyledInput>
+			<StyledInput
+				type='text'
+				name='nombre'
+				placeholder='Nombre'
+				onChange={props.cambiosInput}
+				autoComplete='off'
+				onKeyUp={(e) => props.validar(e, props.regex.nombre)}
+				onBlur={(e) => props.validar(e, props.regex.nombre)}
+				required
+				valido={props.state.nombre.valid}
+			></StyledInput>
 
-            <StyledInput 
-            type="text"
-            placeholder="Edad"
-            name="edad"
-            onChange={props.cambiosInput}
-            autoComplete="off"
-            onKeyUp={(e) => props.validar(e, props.regex.edad)}
-            onBlur={(e) => props.validar(e, props.regex.edad)}
-            required
-            valido={props.state.edad.valid}
-            ></StyledInput>
-            
-            <StyledSelect 
-            name="genero"
-            onChange={props.cambiosInput}
-            value={props.state.genero.value}
-            required
-            validar={props.validar}
-            >
-                <option value="Hombre">Hombre</option>
-                <option value="Mujer">Mujer</option>
-                <option value="Otro">Otro</option>
-            </StyledSelect>
-            
-            <StyledBtn >
-                Registrar
-            </StyledBtn>
+			<StyledInput
+				type='text'
+				placeholder='Apellido'
+				name='apellido'
+				onChange={props.cambiosInput}
+				autoComplete='off'
+				onKeyUp={(e) => props.validar(e, props.regex.apellido)}
+				onBlur={(e) => props.validar(e, props.regex.apellido)}
+				required
+				valido={props.state.apellido.valid}
+			></StyledInput>
 
-        </>
-    );
+			<StyledInput
+				type='text'
+				placeholder='Edad'
+				name='edad'
+				onChange={props.cambiosInput}
+				autoComplete='off'
+				onKeyUp={(e) => props.validar(e, props.regex.edad)}
+				onBlur={(e) => props.validar(e, props.regex.edad)}
+				required
+				valido={props.state.edad.valid}
+			></StyledInput>
+
+			<StyledSelect
+				name='genero'
+				onChange={props.cambiosInput}
+				value={props.state.genero.value}
+				required
+				validar={props.validar}
+			>
+				<option value='Hombre'>Hombre</option>
+				<option value='Mujer'>Mujer</option>
+				<option value='Otro'>Otro</option>
+			</StyledSelect>
+
+			<StyledBtn>Registrar</StyledBtn>
+		</>
+	);
 };
 
 export default NuevoUsuario;
